43e3183f4d1469627ef5b3b096bb7afb91907500
tranvantinh <tinhtv1996@gmail.com>
2024-03-01 13:32:35 +0700

    1

--- Knowledge/MainForm.Designer.cs
+++ Knowledge/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Knowledge
 {
-    partial class Main
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Main";
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 450);
+            panel1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Main";
+            Text = "Main";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
     }
 }
---
+++ Knowledge/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using Accessibility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knowledge
+{
+    public partial class MainForm : Form
+    {
+        private List<Point> objectPositions = new List<Point>();
+
+        private List<System.Drawing.Rectangle>  rectangles = new List<System.Drawing.Rectangle>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeObjects();
+        }
+
+        private void InitializeObjects()
+        {
+            // Thêm các vị trí của đối tượng vào danh sách
+            objectPositions.Add(new Point(100, 100));
+            objectPositions.Add(new Point(200, 150));
+            objectPositions.Add(new Point(150, 250));
+
+            rectangles.Add(new Rectangle(100, 120, 10, 10));
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = panel1.CreateGraphics();
+
+            // Vẽ các đối tượng
+            foreach (Point point in objectPositions)
+            {
+                g.FillEllipse(Brushes.Blue, point.X - 20, point.Y - 20, 40, 40);
+            }
+
+            // Vẽ các liên kết giữa các đối tượng
+            if (objectPositions.Count >= 2)
+            {
+                Pen pen = new Pen(Color.Black, 2);
+                for (int i = 0; i < objectPositions.Count - 1; i++)
+                {
+                    g.DrawLine(pen, objectPositions[i], objectPositions[i + 1]);
+                }
+            }
+        }
+    }
+}
